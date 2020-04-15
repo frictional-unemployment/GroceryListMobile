b9abc69723d6c319fdbecc49a5e326e58c90a208
Anthony Jung <junganthonyy@gmail.com>
2020-04-15 15:16:30 -0700

    Adding Purchased Styling

--- src/components/List/Item/Item.js
+++ src/components/List/Item/Item.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 
 const List = ({
@@ -15,31 +15,33 @@ const List = ({
 
   return (
     <TouchableHighlight
-      style={purchasedStyle}
+      style={[purchasedStyle, purchased ? styles.purchased : styles.unPurchased]}
       onLongPress={() => {
-        console.log('longPress');
         startEdit();
       }}
       onPress={() => {
         togglePurchased();
       }}
       underlayColor="green"
-      activeOpacity={1}
+      activeOpacity={0.2}
     >
-      <View style={styles.item}>
-        <Text style={styles.itemName}>
+      <View style={[styles.item, purchased ? styles.purchased : styles.unPurchased]}>
+        <Text style={[styles.itemName, purchased ? styles.purchased : styles.unPurchased]}>
           {name}
         </Text>
-        <Text style={styles.itemQty}>
-          Item Qty:
+        <Text style={[styles.itemQty, purchased ? styles.purchased : styles.unPurchased]}>
+          Qty:
           {qty}
         </Text>
+        { purchased ? (
+          <Text style={styles.horizontalRule} />
+        ) : null }
       </View>
     </TouchableHighlight>
   );
 };
 
-let styles = {
+let styles = StyleSheet.create({
   item: {
     height: 50,
     borderWidth: 1,
@@ -47,20 +49,47 @@ let styles = {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    width: '100%'
+    width: '100%',
+    flexWrap: 'nowrap',
+    position: 'relative'
   },
   itemName: {
+    height: '100%',
     fontWeight: 'bold',
+    fontSize: 30,
+    overflow: 'hidden',
+    left: '25%',
+    backgroundColor: 'green'
   },
   itemQty: {
+    height: '100%',
     fontWeight: 'bold',
+    marginLeft: 'auto',
+    fontSize: 30,
+    right: '25%'
   },
   touch: {
     width: 100,
     height: 30,
     borderColor: 'black',
     borderWidth: 2
+  },
+  purchased: {
+    backgroundColor: 'grey',
+    textDecorationLine: 'line-through'
+  },
+  unPurchased: {
+    backgroundColor: 'white',
+    textDecorationLine: 'none'
+  },
+  horizontalRule: {
+    width: '100%',
+    height: '50%',
+    borderBottomWidth: 3,
+    alignSelf: 'flex-start',
+    backgroundColor: 'transparent',
+    position: 'absolute'
   }
-};
+});
 
 export default List;
