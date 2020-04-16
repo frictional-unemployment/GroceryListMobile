6c5c11da5a4802b0e9cd21abc7062209e0ab8ede
Anthony Jung <junganthonyy@gmail.com>
2020-04-16 14:03:20 -0700

    Adding Sorting Functionallity

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Button, Modal, View
+  Button, Modal, Picker, ScrollView, View
 } from 'react-native';
 import Item from './Item/Item';
 import NewNameForm from './Forms/NewNameForm';
@@ -14,12 +14,14 @@ class List extends React.Component {
       newNameVisible: false,
       newItemVisible: false,
       editItemVisible: false,
-      editItemIndex: -1
+      editItemIndex: -1,
+      sortingStyle: 'alphabet'
     };
   }
 
   componentDidMount() {
     const { navigation, route } = this.props;
+
     navigation.setOptions({
       title: route.params.listData.listName,
       headerRight: () => (
@@ -30,6 +32,8 @@ class List extends React.Component {
         />
       )
     });
+
+    this.sortListItems(this.state.sortingStyle);
   }
 
   componentDidUpdate() {
@@ -37,6 +41,55 @@ class List extends React.Component {
     navigation.setOptions({
       title: route.params.listData.listName
     });
+  }
+
+  // will sort by unpurchased first then alphabetical
+  // considering adding options to sort differently
+
+  sortListItems(type = 'purchasedLast') {
+    const { navigation, route } = this.props;
+    const { listName, items } = route.params.listData;
+
+    const sortedItems = [...items];
+
+    if (type === 'alphabet') {
+      sortedItems.sort((item1, item2) => {
+        return item1.name.localeCompare(item2.name);
+      });
+    } else if (type === 'purchasedFirst') {
+      sortedItems.sort((item1, item2) => {
+        let output = 0;
+        if (item1.purchased === item2.purchased) {
+          output = item1.name.localeCompare(item2.name);
+        } else if (item1.purchased) {
+          output = -1;
+        } else {
+          output = 1;
+        }
+        return output;
+      });
+    } else if (type === 'purchasedLast') {
+      sortedItems.sort((item1, item2) => {
+        let output = 0;
+        if (item1.purchased === item2.purchased) {
+          output = item1.name.localeCompare(item2.name);
+        } else if (item1.purchased) {
+          output = 1;
+        } else {
+          output = -1;
+        }
+
+        return output;
+      });
+    }
+
+    navigation.setParams({
+      listData: {
+        listName,
+        items: sortedItems
+      }
+    });
+    this.setState
   }
 
   changeNewNameVisible(visibility) {
@@ -75,7 +128,7 @@ class List extends React.Component {
     const { navigation, route } = this.props;
     const item = { name, qty, purchased: false };
     const newListData = { ...route.params.listData };
-    newListData.items.push(item);
+    newListData.items = [item, ...newListData.items];
     navigation.setParams({ listData: newListData });
   }
 
@@ -117,54 +170,63 @@ class List extends React.Component {
     const { newNameVisible, newItemVisible, editItemVisible, editItemIndex } = this.state;
 
     return (
-      <View>
-        {
-          items.map(({ name, qty, purchased }, index) => (
-            <Item
-              name={name}
-              qty={qty}
-              purchased={purchased}
-              key={index}
-              togglePurchased={() => this.togglePurchased(index)}
-              startEdit={() => this.changeEditItemVisible(true, index)}
+      <>
+        <Picker
+          onValueChange={(value) => this.sortListItems(value)}
+        >
+          <Picker.item label="Alphabetical" value="alphabet" />
+          <Picker.item label="Purchased First" value="purchasedFirst" />
+          <Picker.item label="Purchased Last" value="purchasedLast" />
+        </Picker>
+        <ScrollView>
+          {
+            items.map(({ name, qty, purchased }, index) => (
+              <Item
+                name={name}
+                qty={qty}
+                purchased={purchased}
+                key={index}
+                togglePurchased={() => this.togglePurchased(index)}
+                startEdit={() => this.changeEditItemVisible(true, index)}
+              />
+            ))
+          }
+          <Modal
+            visible={newNameVisible}
+            onRequestClose={() => this.changeNewNameVisible(false)}
+          >
+            <NewNameForm
+              commitNewName={(name) => this.changeListName(name)}
+              closeModal={() => this.changeNewNameVisible(false)}
             />
-          ))
-        }
+          </Modal>
+          <Modal
+            visible={newItemVisible}
+            onRequestClose={() => this.changeNewItemVisible(false)}
+          >
+            <NewItemForm
+              commitNewItem={(item) => this.addNewItem(item)}
+              closeModal={() => this.changeNewItemVisible(false)}
+            />
+          </Modal>
+          <Modal
+            visible={editItemVisible}
+            onRequestClose={() => this.changeEditItemVisible(false, -1)}
+          >
+            <EditItemForm
+              index={editItemIndex}
+              item={items[editItemIndex]}
+              saveChanges={(item) => this.editItem(item)}
+              deleteItem={() => this.deleteItem(editItemIndex)}
+            />
+          </Modal>
+        </ScrollView>
         <Button
           title="Add New Item"
           onPress={() => this.changeNewItemVisible(true)}
           color="green"
         />
-        <Modal
-          visible={newNameVisible}
-          onRequestClose={() => this.changeNewNameVisible(false)}
-        >
-          <NewNameForm
-            commitNewName={(name) => this.changeListName(name)}
-            closeModal={() => this.changeNewNameVisible(false)}
-          />
-        </Modal>
-        <Modal
-          visible={newItemVisible}
-          onRequestClose={() => this.changeNewItemVisible(false)}
-        >
-          <NewItemForm
-            commitNewItem={(item) => this.addNewItem(item)}
-            closeModal={() => this.changeNewItemVisible(false)}
-          />
-        </Modal>
-        <Modal
-          visible={editItemVisible}
-          onRequestClose={() => this.changeEditItemVisible(false, -1)}
-        >
-          <EditItemForm
-            index={editItemIndex}
-            item={items[editItemIndex]}
-            saveChanges={(item) => this.editItem(item)}
-            deleteItem={() => this.deleteItem(editItemIndex)}
-          />
-        </Modal>
-      </View>
+      </>
     );
   }
 }
